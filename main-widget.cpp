6a947d2370e98ccfccfded9fe933bae6670a6708
William Tang <galaxyking0419@gmail.com>
2021-02-07 16:42:41 +0800

    UI: Initial UI desgin

--- main-widget.cpp
+++ main-widget.cpp
@@ -2,10 +2,11 @@
 #include "ui_main-widget.h"
 
 MainWidget::MainWidget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::MainWidget)
+    : QWidget(parent), ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+
+    ui->ReceiveSendSplitter->setSizes(QList<int>{350, 60});
 }
 
 MainWidget::~MainWidget()
